45fc1d27419c8237fd0d9be2631e14b3747986d2
nicolas <nicolas.eymenier@gmail.com>
2020-07-21 13:36:18 -0300

    installed chartkick, just need to improve the data source in the controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+    def get_last_date(debenture)
+    # the goal is the find the most recent date for which the fund has data
+    # we choose the last five days in the data base
+    today = Date.today
+    last_dates = []
+    (1..10).each do |number_of_day|
+      previous_day = today - number_of_day
+      existing_date = Calendar.find_by(day: previous_day)
+      last_dates << existing_date unless existing_date.nil?
+    end
+
+    # for each date we check if the fund has data
+    last_dates.each do |last_date|
+      data = DebentureMarketDatum.find_by(debenture: debenture, calendar: last_date)
+      # when the number of DailyDatum for that date == the number of funds then we return the date
+      return last_date if !data.nil?
+    end
+  end
 end
--- app/controllers/debentures_controller.rb
+++ app/controllers/debentures_controller.rb
@@ -1,11 +1,25 @@
 class DebenturesController < ApplicationController
 
-
   def index
     @debentures = Debenture.all
   end
 
+  def show
+    @debenture = Debenture.find(params[:id])
+    # we get the last date
+    @date = get_last_date(@debenture)
+    @chart_prices = get_prices(@debenture)
+  end
 
+  private
 
+  def get_prices(debenture)
+    historical_array = []
 
+    debenture_data = DebentureMarketDatum.where(debenture: debenture)
+
+    historical_array << { name: "Preço", data: debenture_data.map {|t| [t.calendar.day.strftime("%Y-%m"), t.price.round(2)] } }
+    historical_array << { name: "Preço minimo", data: debenture_data.map {|t| [t.calendar.day.strftime("%Y-%m"), t.price_min.round(2)] } }
+    historical_array << { name: "Preço maximo", data: debenture_data.map {|t| [t.calendar.day.strftime("%Y-%m"), t.price_max.round(2)] } }
+  end
 end
